eb6bc25331d938f25a18bc666ffca51c4aad8968
BhavayMangla <89002029+BhavayMangla@users.noreply.github.com>
2022-05-09 21:31:29 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include('connection.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin Login</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="header">
+	<div class="logo"><h2>Blood Bank Management System</h2></div>
+	<div class="nav">
+		<div id="a"><a href="home.php">Home</a></div>
+		<div id="b"><a href="about.php">About As</a></div>
+		<div id="c"><a href="contect.php">Contact Us</a></div>
+		<!-- <div id="d"><a href="index.php">Login</a></div> -->
+	</div>
+</div>
+<div class="banner"><h1 align="center" style="color: black"><br><br><br><br><br><br><br><br><br>
+Blood Bank</h1></div>
+<div class="container">
+     <br>
+     <!-- <h2 align="center" style="color: red">Doctor's Information</h2> -->
+	<br><br>
+	<!-- <center><table border="1px"> -->
+		<!-- <tr>
+			<td width="200px" height="50px" style="color: red"><center><b>Doctor Name</b></center></td>
+			<td width="200px" height="50px" style="color: red"><center><b>Mobile No.</b></center></td>
+			<td width="200px" height="50px" style="color: red"><center><b>Address</b></center></td>
+			<td width="200px" height="50px" style="color: red"><center><b>specialization</b></center></td>
+		</tr> -->
+		<!-- <tr>
+			<td width="200px" height="50px"><center>Dr.Krisha Joshi</center></td>
+			<td width="200px" height="50px"><center>43353535</center></td>
+			<td width="200px" height="50px"><center>M Block, Srm university</center></td>
+			<td width="200px" height="50px"><center>Heart Specialist</center></td>
+		</tr> -->
+	<!-- </table></center> -->
+</div>
+<div class="footer"><br><h2 align="center">MiniprojectDBMS</h2></div>
+</body>
+</html>
